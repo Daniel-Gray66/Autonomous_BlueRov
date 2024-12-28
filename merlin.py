a20a17a7cd146b410b5842449fe80bfee7ca678c
ivanarzola10 <49924082+ivanarzola10@users.noreply.github.com>
2024-12-28 13:58:33 -0800

    Update merlin.py

--- merlin.py
+++ merlin.py
@@ -1,243 +1,72 @@
-
-#!/usr/bin/env python
-"""
-BlueRov video capture class
-"""
-
-import cv2
-import numpy as np
-import gi
-from ultralytics import YOLO
-import os 
-import sys
-
-from testingv2 import (
-    create_connectionlink,
-    arm_vehicle,
-    disarm_vehicle,
-    control_motors
-)
-
-
-
-
-gi.require_version('Gst', '1.0')
-from gi.repository import Gst
-
-
-class Video():
-    """BlueRov video capture class constructor
-
-    Attributes:
-        port (int): Video UDP port
-        video_codec (string): Source h264 parser
-        video_decode (string): Transform YUV (12bits) to BGR (24bits)
-        video_pipe (object): GStreamer top-level pipeline
-        video_sink (object): Gstreamer sink element
-        video_sink_conf (string): Sink configuration
-        video_source (string): Udp source ip and port
-        latest_frame (np.ndarray): Latest retrieved video frame
-    """
-
-    def __init__(self, port=5600):
-        """Summary
-
-        Args:
-            port (int, optional): UDP port
-        """
-
-        Gst.init(None)
-
-        self.port = port
-        self.latest_frame = self._new_frame = None
-
-        # [Software component diagram](https://www.ardusub.com/software/components.html)
-        # UDP video stream (:5600)
-        self.video_source = 'udpsrc port={} ! application/x-rtp, media=video, payload=96, width=1280, height=720'.format(self.port)
-        # [Rasp raw image](http://picamera.readthedocs.io/en/release-0.7/recipes2.html#raw-image-capture-yuv-format)
-        # Cam -> CSI-2 -> H264 Raw (YUV 4-4-4 (12bits) I420)
-        self.video_codec = '! application/x-rtp, payload=96 ! rtph264depay ! h264parse ! avdec_h264'
-        # Python don't have nibble, convert YUV nibbles (4-4-4) to OpenCV standard BGR bytes (8-8-8)
-        self.video_decode = \
-            '! decodebin ! videoconvert ! video/x-raw,format=(string)BGR ! videoconvert'
-        # Create a sink to get data
-        self.video_sink_conf = \
-            '! appsink emit-signals=true sync=false max-buffers=2 drop=true'
-
-        self.video_pipe = None
-        self.video_sink = None
-
-        #GPT CODE
-        self.video_source = 'udpsrc port={}'.format(self.port)
-        self.video_codec = '! application/x-rtp, payload=96 ! rtph264depay ! h264parse ! avdec_h264'
-        self.video_decode = '! videoconvert ! video/x-raw, format=(string)BGR ! videoconvert'
-        self.video_sink_conf = '! appsink emit-signals=true sync=false max-buffers=2 drop=true'
-
-        self.run()
-
-    def start_gst(self, config=None):
-        """ Start gstreamer pipeline and sink
-        Pipeline description list e.g:
-            [
-                'videotestsrc ! decodebin', \
-                '! videoconvert ! video/x-raw,format=(string)BGR ! videoconvert',
-                '! appsink'
-            ]
-
-        Args:
-            config (list, optional): Gstreamer pileline description list
-        """
-
-        if not config:
-            config =  [
-                '{} ! capsfilter caps="application/x-rtp, media=video, payload=96"'.format(self.video_source),
-                self.video_codec,
-                self.video_decode,
-                self.video_sink_conf
-            ]
-
-        command = ' '.join(config)
-        self.video_pipe = Gst.parse_launch(command)
-        self.video_pipe.set_state(Gst.State.PLAYING)
-        self.video_sink = self.video_pipe.get_by_name('appsink0')
-
-    @staticmethod
-    def gst_to_opencv(sample):
-        """Transform byte array into np array
-
-        Args:
-            sample (TYPE): Description
-
-        Returns:
-            TYPE: Description
-        """
-        buf = sample.get_buffer()
-        caps_structure = sample.get_caps().get_structure(0)
-        array = np.ndarray(
-            (
-                caps_structure.get_value('height'),
-                caps_structure.get_value('width'),
-                3
-            ),
-            buffer=buf.extract_dup(0, buf.get_size()), dtype=np.uint8)
-        return array
-
-    def frame(self):
-        """ Get Frame
-
-        Returns:
-            np.ndarray: latest retrieved image frame
-        """
-        if self.frame_available:
-            self.latest_frame = self._new_frame
-            # reset to indicate latest frame has been 'consumed'
-            self._new_frame = None
-        return self.latest_frame
-
-    def frame_available(self):
-        """Check if a new frame is available
-
-        Returns:
-            bool: true if a new frame is available
-        """
-        return self._new_frame is not None
-
-    def run(self):
-        """ Get frame to update _new_frame
-        """
-
-        self.start_gst(
-            [
-                self.video_source,
-                self.video_codec,
-                self.video_decode,
-                self.video_sink_conf
-            ])
-
-        self.video_sink.connect('new-sample', self.callback)
-
-    def callback(self, sink):
-        sample = sink.emit('pull-sample')
-        self._new_frame = self.gst_to_opencv(sample)
-
-        return Gst.FlowReturn.OK
-
-
 if __name__ == '__main__':
-    sys.stdout = open (os.devnull,'w')
-    # Create the video object
-    # Add port= if is necessary to use a different one
+    # Comment this out so we can see print statements:
+    # sys.stdout = open(os.devnull, 'w')  
+
+    # 1. Load YOLO model
     model = YOLO('yolov8n.pt')
-    print("Created the model")
+    print("Created the YOLO model")
 
+    # 2. Set up video
     video = Video()
+    print("Video object created.")
 
-     # 2. Connect to ROV
+    # 3. Connect to ROV
     master = create_connectionlink()
-    print("connedction created")
+    print("Connection created.")
     if not master:
         sys.exit("Failed to establish MAVLink connection. Exiting.")
 
-    # 3. Arm vehicle
+    # 4. Arm vehicle
     if not arm_vehicle(master):
         sys.exit("Vehicle refused to arm. Exiting.")
-    
+    print("Vehicle armed.")
 
+    # 5. Wait for frames
     print('Initialising stream...')
     waited = 0
     while not video.frame_available():
         waited += 1
-        print('\r  Frame not available (x{})'.format(waited), end='')
+        print(f'\r  Frame not available (x{waited})', end='')
         cv2.waitKey(30)
     print('\nSuccess!\nStarting streaming - press "q" to quit.')
 
     while True:
-        # Wait for the next frame to become available
         if video.frame_available():
-            # Only retrieve and display a frame if it's new
             frame = video.frame()
-            results = model(frame)
+            if frame is None:
+                continue  # or break, depending on your needs
 
+            results = model(frame)
             annotated_frame = results[0].plot()
 
-            detected_bottle = False
-
-             # results[0].boxes has the bounding boxes
+            # Debug: see if YOLO is detecting anything
             boxes = results[0].boxes
-
-                # Each box has .cls (class index), .conf (confidence), etc.
-                # results[0].names is a dict {class_index: class_name}
+            detected_person = False
             for box in boxes:
                 class_id = int(box.cls[0])
                 class_name = results[0].names[class_id]
-                # If the model's class name is 'bottle'
+                print(f"Detected: {class_name}")
                 if class_name == "person":
-                    detected_bottle = True
-                    break  # We only need one bottle to move
+                    detected_person = True
+                    break
 
-                # ------------------------------------------------------
-                # MOTOR CONTROL: Move only if a bottle is detected
-                # ------------------------------------------------------
-            if detected_bottle:
-                print("Bottle detected! Moving forward...")
-                control_motors(master, 1500, 1500, 1500, 1600, 1500, 1500,1500)
+            # Motor control
+            if detected_person:
+                print("Person detected! Moving forward...")
+                # Make sure to pass correct number of motor values:
+                control_motors(master, 1500, 1500, 1500, 1600, 1500, 1500)
             else:
-                    # No bottle => stay neutral
-                control_motors(master, 1500, 1500, 1500, 1500, 1500, 1500,1500)
+                # No person => neutral
+                control_motors(master, 1500, 1500, 1500, 1500, 1500, 1500)
+
             cv2.imshow('Fishy device', annotated_frame)
 
-        # Allow frame to display, and check if user wants to quit
+        # Quit on 'q'
         if cv2.waitKey(1) & 0xFF == ord('q'):
-            control_motors(
-                master,
-                1500,
-                1500,
-                1500,
-                1500
-            )
+            # Stop motors
+            control_motors(master, 1500, 1500, 1500, 1500, 1500, 1500)
             disarm_vehicle(master)
             break
 
-    sys.stdout.close()
-    sys.stdout = sys.__stdout__
-
+    cv2.destroyAllWindows()
+    print("Exiting program.")
